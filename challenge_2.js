22ffed3c899c3694b43c11ad0dd4a74edf59ce15
duyvu997 <duy.vu@watatek.com>
2025-05-15 04:35:20 +0700

    feat: challenge 2

---
+++ challenge_2.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ **/
+function sumOfDistancesInTree(n, edges) {
+    const tree = Array.from({ length: n }, () => []);
+    for (const [u, v] of edges) {
+        tree[u].push(v);
+        tree[v].push(u);
+    }
+
+    const bfs = (start) => {
+        const visited = Array(n).fill(false);
+        const queue = [[start, 0]];
+        let sum = 0;
+        visited[start] = true;
+
+        while (queue.length) {
+            const [node, dist] = queue.shift();
+            sum += dist;
+
+            for (const neighbor of tree[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push([neighbor, dist + 1]);
+                }
+            }
+        }
+
+        return sum;
+    };
+
+    const result = [];
+    for (let i = 0; i < n; i++) {
+        result.push(bfs(i)*2);
+    }
+    console.log(result);
+    
+    return result
+}
+
+
+sumOfDistancesInTree(10, [
+    [0, 1],
+    [0, 2],
+    [1, 3],
+    [1, 4],
+    [2, 5],
+    [2, 6],
+    [3, 7],
+    [3, 8],
+    [4, 9]
+]);
